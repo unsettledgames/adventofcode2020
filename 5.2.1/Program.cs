dd05b69e61be3bbfd16d27f68b9aabbc62b3c85b
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-12-05 12:03:44 +0100

    Solved 5.2

--- 5.2.1/Program.cs
+++ 5.2.1/Program.cs
@@ -7,58 +7,10 @@ namespace _5._2._1
     {
         private static int ParseSeat(String id)
         {
-			int curr_max_row = 127;
-			int curr_min_row = 0;
-			int curr_min_col = 0;
-			int curr_max_col = 7;
-
-			int row, col;
-
-			bool keep_low_row = false;
-			bool keep_low_col = false;
-
-			for (int i = 0; i < id.Length; i++)
-			{
-				switch (id[i])
-				{
-					case 'F':
-						curr_max_row = (curr_min_row + curr_max_row) / 2;
-						keep_low_row = true;
-						break;
-					case 'B':
-						curr_min_row = (curr_min_row + curr_max_row) / 2;
-						keep_low_row = false;
-						break;
-					case 'L':
-						curr_max_col = (curr_min_col + curr_max_col) / 2;
-						keep_low_col = true;
-						break;
-					case 'R':
-						curr_min_col = (curr_min_col + curr_max_col) / 2;
-						keep_low_col = false;
-						break;
-				}
-			}
-
-			if (keep_low_row)
-			{
-				row = curr_min_row;
-			}
-			else
-			{
-				row = curr_max_row;
-			}
-
-			if (keep_low_col)
-			{
-				col = curr_min_col;
-			}
-			else
-			{
-				col = curr_max_col;
-			}
-
-			return row * 8 + col;
+            int row = Convert.ToInt32(id.Substring(0, 7).Replace('F', '0').Replace('B', '1'), 2);
+            int col = Convert.ToInt32(id.Substring(7, 3).Replace('L', '0').Replace('R', '1'), 2);
+            
+            return row * 8 + col;
 		}
 
         static void Main(string[] args)
@@ -75,7 +27,7 @@ namespace _5._2._1
 
 			for (int i=1; i<127; i++)
             {
-				for (int j=0; i<8; j++)
+				for (int j=0; j<8; j++)
                 {
 					int id = i * 8 + j;
 
