c47a6bb5578b96b77ee30fd4cdeacb1ec6267dd1
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-12-10 10:24:21 +0100

    Solved day 10 puzzles

---
+++ 10.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = System.IO.File.ReadAllLines("input.txt");
+            Dictionary<int, int> differences = new Dictionary<int, int>();
+            List<int> jolts = new List<int>();
+
+            differences.Add(1, 0);
+            differences.Add(2, 0);
+            differences.Add(3, 0);
+
+            foreach (String line in lines)
+            {
+                jolts.Add(Int32.Parse(line));
+            }
+
+            jolts.Sort();
+
+            // Charger output
+            differences[jolts[0]]++;
+
+            for (int i=1; i<jolts.Count; i++)
+            {
+                differences[jolts[i] - jolts[i - 1]]++;
+            }
+
+            // Device input
+            differences[3]++;
+
+            Console.WriteLine(differences[3] * differences[1]);
+        }
+    }
+}
---
+++ 10.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = System.IO.File.ReadAllLines("input.txt");
+            List<int> jolts = new List<int>();
+            BigInteger ret = new BigInteger(1);
+            Dictionary<int, BigInteger> paths = new Dictionary<int, BigInteger>();
+
+            foreach (String line in lines)
+            {
+                int n = Int32.Parse(line);
+
+                if (!jolts.Contains(n))
+                {
+                    jolts.Add(n);
+                    paths[n] = 0;
+                }
+            }
+
+            jolts.Add(0);
+            jolts.Sort();
+            jolts.Add(jolts[jolts.Count - 1] + 3);
+            paths[0] = 1;
+            paths[jolts[jolts.Count - 1]] = 0;
+
+
+            for (int i=0; i<jolts.Count - 1; i++)
+            {
+                int j = i + 1;
+
+                while (j < jolts.Count && jolts[j] - jolts[i] <= 3)
+                {
+                    paths[jolts[j]] += paths[jolts[i]];
+                    j++;
+                }
+            }
+
+            Console.WriteLine(paths[jolts[jolts.Count - 1]]);
+        }
+    }
+}
